43a685c8c6f74a7d59d0e3676f5b262473f70bc4
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-01-07 11:08:51 +0300

    create project rep

---
+++ src/app/components/common/form/multiSelectField.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Select from "react-select";
+import PropTypes from "prop-types";
+
+const MultiSelectField = ({ options, onChange, name, label, defaultValue }) => {
+    const defaultValueToArray = defaultValue.map((value) => ({
+        label: value.name,
+        value: value._id
+    }));
+    const optionsArray =
+        !Array.isArray(options) && typeof options === "object"
+            ? Object.keys(options).map((optionName) => ({
+                  label: options[optionName].name,
+                  value: options[optionName]._id
+              }))
+            : options;
+
+    const handleChange = (value) => {
+        onChange({ name: name, value });
+    };
+    return (
+        <div className="mb-4">
+            <label className="form-label">{label}</label>
+            <Select
+                isMulti
+                defaultValue={defaultValueToArray}
+                closeMenuOnSelect={false}
+                options={optionsArray}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                onChange={handleChange}
+                name={name}
+            />
+        </div>
+    );
+};
+MultiSelectField.propTypes = {
+    options: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    onChange: PropTypes.func,
+    name: PropTypes.string,
+    label: PropTypes.string,
+    defaultValue: PropTypes.array
+};
+
+export default MultiSelectField;
